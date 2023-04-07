b9d90630ac7f29a14fa672b8c4dd04a1b4740366
seansqlifting29 <styndale.gtalum@gmail.com>
2023-04-07 15:18:29 +0800

    Change project No.1 spacing

---
+++ ProjectNo.1/app.js
@@ -0,0 +1,388 @@
+import { formioStatsCheckbox, formioStatsCheckbox as statsCheckbox } from './node_modules/formio-results-statistics/index.js'
+var formioResult1 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult2 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult3 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult4 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult5 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult6 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult7 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult8 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult9 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult10 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult11 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult12 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult13 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult14 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult15 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult16 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult17 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult18 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult19 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult20 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": false,
+        "textArea": "Toning arms worked very well",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult21 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": true,
+        "married": false,
+        "single": true,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": true,
+        "textArea": "No changes needed",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult22 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": true,
+        "textArea": "work on squat form",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult23 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need help in surf techniques",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult24 = {
+    "data": {
+        "under18": true,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": true,
+        "textArea": "Need to surf",
+        "submit": false
+    },
+    "metadata": {}
+}
+
+var formioResults = [formioResult1, formioResult2, formioResult3, formioResult4, formioResult5, formioResult6, formioResult7, formioResult8, formioResult9, formioResult10, formioResult11, formioResult12, formioResult13, formioResult14, formioResult15, formioResult16, formioResult17, formioResult18, formioResult19, formioResult20, formioResult21, formioResult22, formioResult23, formioResult24];
+statsCheckbox(formioResults, "male");
